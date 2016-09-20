54c997406fe0eada0de75531811fee928e813cea
patsykakaz <philippe@lesidecar.fr>
2016-09-20 15:19:48 +0200

    forced staging of main.css and main.js

---
+++ static/js/main.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+
+    if($('#model_type').val() == 'Person'){
+       $("input[name='job']").fadeIn();
+    }
+    if($("#country").val() == "France"){
+        $("input[name='zipcode']").fadeIn();
+    }
+
+    $('#model_type').change(function(){
+        if($(this).val() == "Person"){
+            $("input[name='job']").fadeIn();
+        }else{
+            $("input[name='job']").fadeOut();
+        }
+    });
+
+    $("#country").change(function(){
+        if($(this).val() == "France"){
+            $("input[name='zipcode']").fadeIn();
+        }else{
+            $("input[name='zipcode']").fadeOut();
+        }
+    });
+
+    $('#topic').change(function(){
+        $('select[name="subtopic"]').css('display','none');
+        rel = $('#topic option:selected').val();
+        target = $('select[name="subtopic"][rel="'+rel+'"]');
+        target.fadeIn();
+    });
+
+});
+
+
+
+
+
